891c24969635881bcfd8f801f041a6011284574d
Migara Karunarathne <migara1225@gmail.com>
2022-06-17 02:03:53 +0530

    mobile finalize

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -42,41 +42,44 @@ export default function Header() {
 	};
 
 	return (
-		<header className="header">
-			<div className="brand">
-				<img src={logo} className="brand__logo" alt="logo" />
-				<div
-					className={`menu-toggle ${
-						isShowMobileMenu ? "menu-toggle__activate" : ""
-					}`}
-					onClick={toggleClass}
-				>
-					<span className="bar"></span>
-					<span className="bar"></span>
-					<span className="bar"></span>
+		<>
+			<header className="header">
+				<div className="brand">
+					<img src={logo} className="brand__logo" alt="logo" />
+					<div
+						className={`menu-toggle ${
+							isShowMobileMenu ? "menu-toggle__activate" : ""
+						}`}
+						onClick={toggleClass}
+					>
+						<span className="bar"></span>
+						<span className="bar"></span>
+						<span className="bar"></span>
+					</div>
 				</div>
-			</div>
-			<div
-				className={`navigation ${
-					isShowMobileMenu ? "navigation--active" : ""
-				}`}
-			>
-				<ul className="navigation__list">
-					{localJson.map((data) => (
-						<li
-							className={`navigation__list-item navigation__list-item--${data.name.toLowerCase()}`}
-							key={data.name}
-						>
-							<Link
-								to={`/${data.name.toLowerCase()}`}
-								onClick={toggleClass}
+				<div className="divider"></div>
+				<div
+					className={`navigation ${
+						isShowMobileMenu ? "navigation--active" : ""
+					}`}
+				>
+					<ul className="navigation__list">
+						{localJson.map((data) => (
+							<li
+								className={`navigation__list-item navigation__list-item--${data.name.toLowerCase()}`}
+								key={data.name}
 							>
-								{data.name}
-							</Link>
-						</li>
-					))}
-				</ul>
-			</div>
-		</header>
+								<Link
+									to={`/${data.name.toLowerCase()}`}
+									onClick={toggleClass}
+								>
+									{data.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			</header>
+		</>
 	);
 }
